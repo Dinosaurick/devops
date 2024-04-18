006baff862b9d9fc8f6a1b7921cda99cf6ff63d0
Dinosaurick <dima.chern1@mail.ru>
2024-04-18 23:56:31 +0300

    ficha

--- python/app.py
+++ python/app.py
@@ -2,6 +2,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+counter = 0
+
+@app.route('/')
+def index():
+    global counter
+    counter += 1
+    return f"Цифра: {counter}"
+
 @app.route('/')
 def hello_world():
     return 'Hello World! 9.00)('
